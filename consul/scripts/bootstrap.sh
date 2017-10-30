3845490e81d88b89d4f574f0bf03e09a360bc79d
Stephen Leyva <sl_leyva25@yahoo.com>
2017-10-30 10:17:53 -0400

    Multibuild (#8)
    * Added Kubernetes deployment spec
    
    * Using Mutli Stage Build for images

--- consul/scripts/bootstrap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-
-# Bootstrap consul and vault
-export VAULT_ADDR='http://127.0.0.1:8200'
-consul agent -server -bootstrap-expect 1 -data-dir /tmp/consul -client 0.0.0.0 -bind 127.0.0.1
